16e40dae6d53aa767f31411167a6f071a2cb5b23
Beata Bień <beatabien22@gmail.com>
2022-10-17 13:43:58 +0200

    Add BottomNavigationBar

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,7 +1,8 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     Key? key,
     required this.user,
@@ -10,25 +11,60 @@ class HomePage extends StatelessWidget {
   final User user;
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+var currentIndex = 0;
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Jesteś zalogowany jako ${user.email}',
-            ),
-            ElevatedButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              child: const Text(
-                'Wyloguj się',
+      body: Builder(builder: (context) {
+        if (currentIndex == 0) {
+          return const Center(
+            child: Text('Jeden'),
+          );
+        }
+        if (currentIndex == 1) {
+          return const Center(
+            child: Text('Dwa'),
+          );
+        }
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Jesteś zalogowany jako ${widget.user.email}',
               ),
-            ),
-          ],
-        ),
+              ElevatedButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                child: const Text(
+                  'Wyloguj się',
+                ),
+              ),
+            ],
+          ),
+        );
+      }),
+      bottomNavigationBar: CurvedNavigationBar(
+        height: 55,
+        backgroundColor: Colors.white,
+        color: const Color.fromARGB(255, 209, 167, 216),
+        animationDuration: const Duration(milliseconds: 300),
+        onTap: (newIndex) {
+          setState(() {
+            currentIndex = newIndex;
+          });
+        },
+        items: const [
+          Icon(Icons.home, color: Colors.white, size: 30),
+          Icon(Icons.sunny, color: Colors.white, size: 30),
+          Icon(Icons.person, color: Colors.white, size: 30),
+        ],
       ),
     );
   }
