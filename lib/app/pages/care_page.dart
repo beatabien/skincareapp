557a805e3db83b34c377c5dad09e3d75eea8d0df
Beata Bień <beatabien22@gmail.com>
2022-10-19 11:32:43 +0200

    Add four Containers to CarePage

--- lib/app/pages/care_page.dart
+++ lib/app/pages/care_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CarePage extends StatelessWidget {
   const CarePage({
@@ -8,10 +9,85 @@ class CarePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SkinCare'),
-        backgroundColor: const Color.fromARGB(255, 209, 167, 216),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('SkinCare'),
+          backgroundColor: const Color.fromARGB(255, 209, 167, 216),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: ListView(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 220,
+                    width: 160,
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30),
+                            bottomRight: Radius.circular(30)),
+                        image: DecorationImage(
+                          image: AssetImage(
+                            'images/morning.jpg',
+                          ),
+                          fit: BoxFit.cover,
+                        )),
+                    child: Center(
+                      child: Text(
+                        'Rano',
+                        style: GoogleFonts.manrope(fontSize: 30),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 220,
+                    width: 160,
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30),
+                            bottomRight: Radius.circular(30)),
+                        image: DecorationImage(
+                          image: AssetImage(
+                            'images/evening.jpg',
+                          ),
+                          fit: BoxFit.cover,
+                        )),
+                    child: Center(
+                      child: Text(
+                        'Wieczór',
+                        style: GoogleFonts.manrope(fontSize: 30),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
+              Container(
+                height: 150,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                      bottomRight: Radius.circular(30)),
+                  color: Color.fromARGB(255, 209, 167, 216),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Container(
+                height: 150,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                      bottomRight: Radius.circular(30)),
+                  color: Color.fromARGB(255, 209, 167, 216),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
