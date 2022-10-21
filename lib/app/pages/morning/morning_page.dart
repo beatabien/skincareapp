9d2de0f039f6e4aae3d7022b8d734e1463431e0a
Beata Bień <beatabien22@gmail.com>
2022-10-21 14:53:12 +0200

    Add StreamBuilder to MorningPage

--- lib/app/pages/morning/morning_page.dart
+++ lib/app/pages/morning/morning_page.dart
@@ -1,12 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MorningPage extends StatelessWidget {
   const MorningPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Poranna pielęgnacja'),
+          backgroundColor: const Color.fromARGB(255, 209, 167, 216),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              stream:
+                  FirebaseFirestore.instance.collection('morning').snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return const Center(child: Text('Something went wrong'));
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: Text('Loading'));
+                }
+                final documents = snapshot.data!.docs;
+                return ListView(
+                  children: [MorningWidget(documents[0]['title'])],
+                );
+              }),
+        ));
+  }
+}
+
+class MorningWidget extends StatelessWidget {
+  const MorningWidget(
+    this.title, {
+    Key? key,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color.fromARGB(255, 155, 106, 164),
+      padding: const EdgeInsets.all(20),
+      child: Text(
+        title,
+        style: GoogleFonts.manrope(
+          fontSize: 20,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
