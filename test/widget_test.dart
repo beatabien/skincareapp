b9aa65657d58fd9d22c8b99c78192ce8b3b48a62
Beata Bień <beatabien22@gmail.com>
2022-11-22 12:59:04 +0100

    Move MyApp to app.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:skincareapp/main.dart';
+import 'package:skincareapp/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
