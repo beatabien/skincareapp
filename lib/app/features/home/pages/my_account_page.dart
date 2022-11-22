316b69065006f92246380eaf5074a3502c414fc0
Beata Bień <beatabien22@gmail.com>
2022-11-22 13:50:46 +0100

    Implement RootCubit

--- lib/app/features/home/pages/my_account_page.dart
+++ lib/app/features/home/pages/my_account_page.dart
@@ -1,5 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skincareapp/app/cubit/root_cubit.dart';
 
 class MyAccountPage extends StatelessWidget {
   const MyAccountPage({
@@ -20,7 +22,7 @@ class MyAccountPage extends StatelessWidget {
           ),
           ElevatedButton(
             onPressed: () {
-              FirebaseAuth.instance.signOut();
+              context.read<RootCubit>().signOut();
             },
             child: const Text(
               'Wyloguj się',
